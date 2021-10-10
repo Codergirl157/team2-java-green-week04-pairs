0d1b86f657f4d0c761fb0d22c7afcf18b7fbd688
Mohammed Fofanah <mohammedfofanah@gmail.com>
2021-10-09 20:19:38 -0400

    pairs update

--- m1-capstone-java-cateringsystem/src/main/java/com/techelevator/BalanceAccount.java
+++ m1-capstone-java-cateringsystem/src/main/java/com/techelevator/BalanceAccount.java
@@ -4,9 +4,7 @@ public class BalanceAccount {
 
     private double accountBalance = 0.00;
 
-    public void setAccountBalance(double accountBalance) {
-        this.accountBalance = accountBalance;
-    }
+
 
     public double getAccountBalance() {
         return accountBalance;
--- m1-capstone-java-cateringsystem/src/main/java/com/techelevator/CateringSystemCLI.java
+++ m1-capstone-java-cateringsystem/src/main/java/com/techelevator/CateringSystemCLI.java
@@ -5,59 +5,70 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CateringSystemCLI {
+    ProductInventory inventory;
+    UserInterface userInterface;
+    BalanceAccount balanceAccount;
 
 
-	public static void main(String[] args) {
-		CateringSystemCLI cli = new CateringSystemCLI();
-		cli.run();
-	}
+    public static void main(String[] args) {
+        CateringSystemCLI cli = new CateringSystemCLI();
+        cli.run();
+    }
 
 
-	public void run() {
-		//probably should do stuff here... ;-)
+    public void run() {
+        //probably should do stuff here... ;-)
 
-		//create an instance of the inventory so we can use it to get data
-		ProductInventory inventory = new ProductInventory("C:\\Users\\Student\\source\\repos\\pairs\\team2-java-green-week04-pairs\\m1-capstone-java-cateringsystem\\cateringsystem.csv");
-		UserInterface userInterface = new UserInterface();
-		BalanceAccount balanceAccount = new BalanceAccount();
-		//while loop, if statements
+        //create an instance of the inventory so we can use it to get data
+        inventory = new ProductInventory("C:\\Users\\Student\\source\\repos\\pairs\\team2-java-green-week04-pairs\\m1-capstone-java-cateringsystem\\cateringsystem.csv");
+        userInterface = new UserInterface();
+        balanceAccount = new BalanceAccount();
+        //while loop, if statements
 
-		boolean isRunning = true;
-		while (isRunning) {
-			String choice = userInterface.printMenu();
+        boolean isRunning = true;
+        while (isRunning) {
+            String choice = userInterface.printMenu();
 
-			if (choice.equals("1")) {
-				//display catering menu
-				List<MenuItem> results = inventory.retrieveListOfItems();
-				//pass to userInterface
-				userInterface.printMenu(results);
+            if (choice.equals("1")) {
+                //display catering menu
+                List<MenuItem> results = inventory.retrieveListOfItems();
+                //pass to userInterface
+                userInterface.printMenu(results);
 
-			} else if (choice.equals("2")) {
-				userInterface.printsubMenu();
+            } else if (choice.equals("2")) {
 
-
-
-
-				//display submenu
+                subMenu();
+                //display submenu
 
 
-
-				}
-					else if (choice.equals("3")) {
-					//Quit
-					isRunning = false;
-					System.out.println("Thank you, and have a great day!");
-					System.exit(0);
-				} else {
-					//error Invalid Option
-
-				}
-
-			}
+            } else if (choice.equals("3")) {
+                //Quit
+                isRunning = false;
+                System.out.println("Thank you, and have a great day!");
+                System.exit(0);
+            } else {
+                //error Invalid Option
+                System.out.println("Please enter a valid selection");
+            }
 
 
-		}
+        }
 
 
-	}
+    }
+    private void subMenu()  {
 
+
+        boolean running = true;
+
+        while (running) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            String subMenuChoice = userInterface.printsubMenu();
+
+            if (subMenuChoice.equalsIgnoreCase("1")) {
+                System.out.println("You chose add money, please enter the amount you will like to add.");
+                double credit = Double.parseDouble(scanner.nextLine());
+            }
+        }    }    }
